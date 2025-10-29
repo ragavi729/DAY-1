9e3213cacced8a7e01aa74ab788c478066253689
ragavi729 <ragavik43@gmail.com>
2025-10-29 14:07:28 +0530

    Add files via upload

---
+++ day 1 pbslvQ1.cpp
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+int main() {
+    char word[100];
+    printf("hello ");
+    scanf("%s", word);
+    printf("hello %s\n", word);
+    return 0;
+}
+	
+
+
+	 
+	 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
