468db854ef21cc6d78922a51db4ab9975be86c50
ragavi729 <ragavik43@gmail.com>
2025-11-10 11:15:20 +0530

    Add files via upload

---
+++ day 1 pbslv Q6.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    float number;
+    printf("Enter a fractional (float) number: ");
+    scanf("%f", &number);
+    printf("You entered: %.2f\n", number);
+    return 0;
+}
